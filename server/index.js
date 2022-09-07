acd7cbd1c289a751343c5f74f00223bdf5d8510a
Gedeon Kakona <gedeonkakona@gmail.com>
2022-09-07 08:23:46 +0200

    creating schema, models, db

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const colors = require('colors');
 const { graphqlHTTP } = require('express-graphql');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const connectDB = require('./config/db');
 const schema = require('./schema/schema');
 
 const app = express();
+//connecting my database
+connectDB();
 
 app.use(
   '/graphql',
