efc977d318bdd0aa4fe295893ddee8716482c490
Gedeon Kakona <gedeonkakona@gmail.com>
2022-09-08 14:37:53 +0200

    adding frontend

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './assets/miz.png';
+
+const Header = () => {
+  return (
+    <nav className="navbar bg-light mb-4 p-0">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          <div className="d-flex">
+            <img src={logo} className="d-inline-block mr-2" alt="Miz1" />
+            <div>Project Management</div>
+          </div>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const colors = require('colors');
 const { graphqlHTTP } = require('express-graphql');
+const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const connectDB = require('./config/db');
@@ -9,6 +10,7 @@ const schema = require('./schema/schema');
 const app = express();
 //connecting my database
 connectDB();
+app.use(cors());
 
 app.use(
   '/graphql',
